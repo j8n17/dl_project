ea091509b6e3e2f12149d0eb87b5430e85966a12
j8n17 <j3837301@gmail.com>
2024-01-04 10:45:29 +0900

    Add CrossEntropyLoss

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,8 @@
 import torch.nn.functional as F
-
+import torch.nn as nn
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
+
+def ce_loss(output, target):
+    return nn.CrossEntropyLoss()(output, target)
