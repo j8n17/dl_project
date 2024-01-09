6e58d7b6bc994f81a31c6679df2052b5cfc4b174
j8n17 <j3837301@gmail.com>
2024-01-09 10:09:07 +0900

    add rgb_diff

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -45,12 +45,13 @@ class CustomSampler(Sampler):
         return self.num_parent_dir
 
 class CustomDataset(Dataset):
-    def __init__(self, root_path, data_txt, augment):
+    def __init__(self, root_path, data_txt, augment, modality):
         # 데이터셋 초기화
         # 예: 파일 경로 목록 로드, 변환 초기화 등
         self.root_path = root_path
         self.data_txt = data_txt
         self.augment = augment
+        self.modality = modality
 
         with open(os.path.join(self.root_path, self.data_txt), 'r') as file:
             self.data = file.readlines()
@@ -64,7 +65,7 @@ class CustomDataset(Dataset):
         # 예: 이미지를 로드하고 변환 적용
         data = self.data[idx].split()
         frame_folder = os.path.join(self.root_path, data[0])
-        clips = int(data[1])
+        self.clips = int(data[1])
         label = torch.tensor(int(data[2]))
 
         frames_path = sorted(glob(os.path.join(frame_folder, '*.jpg'))) # 오름차순으로 정렬
@@ -76,14 +77,25 @@ class CustomDataset(Dataset):
         frames = np.concatenate(frame_ls, axis=2)
         frames = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406] * clips, std=[0.229, 0.224, 0.225] * clips)])(frames)
+            transforms.Normalize(mean=[0.485, 0.456, 0.406] * self.clips, std=[0.229, 0.224, 0.225] * self.clips)])(frames)
         frames = self.augment(frames)
-        frames = frames.reshape((clips, 3) + frames.size()[1:])
+        frames = frames.reshape((self.clips, 3) + frames.size()[1:])
+
+        if self.modality == 'rgb_diff':
+            frames = self._get_diff(frames)
 
         return frames, label
+    
+    def _get_diff(self, input, keep_rgb=False):
+        new_data = input.clone()
 
+        for x in reversed(list(range(1, self.clips))):
+            new_data[x, :, :, :] = input[x, :, :, :] - input[x - 1, :, :, :]
+
+        return new_data if keep_rgb else new_data[1:, :, :, :]
+    
 class CustomDataLoader(BaseDataLoader):
-    def __init__(self, root_path, data_txt, batch_size, shuffle=True, num_workers=1, mode='train'):
+    def __init__(self, root_path, data_txt, batch_size, shuffle=True, num_workers=1, mode='train', modality='rgb'):
         aug = transforms.Compose([
             transforms.CenterCrop((720, 960)),
             transforms.Pad((0, 240, 0, 240), fill=0), # (좌, 상, 우, 하), (960, 960)으로 패딩
@@ -92,7 +104,7 @@ class CustomDataLoader(BaseDataLoader):
         ])
         self.root_path = root_path
         self.data_txt = data_txt
-        self.dataset = CustomDataset(self.root_path, self.data_txt, augment=aug)
+        self.dataset = CustomDataset(self.root_path, self.data_txt, augment=aug, modality=modality)
         if mode == 'train':
             self.sampler = CustomSampler(self.root_path, self.data_txt, shuffle=shuffle)
             shuffle = False # CustomSampler를 사용할 때는 DataLoader의 shuffle은 False여야 함.
